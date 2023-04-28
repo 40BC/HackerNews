9ca2d0202ccc6f363203dee843e6258d340d10ce
Brandon Criss <bcriss249@gmail.com>
2023-04-28 12:39:13 -0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by bjorn on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
