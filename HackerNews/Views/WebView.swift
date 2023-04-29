5be0433b6aaa3fd7d000c2aba4426e3a21bdcc7e
Brandon Criss <bcriss249@gmail.com>
2023-04-29 15:45:15 -0500

    Added WebView and functionality to load url from HackerNews

---
+++ HackerNews/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  HackerNews
+//
+//  Created by bjorn on 4/29/23.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        print("URL \(urlString ?? "no url")")
+        if let safeUrl = urlString {
+            if let url = URL(string: safeUrl) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
